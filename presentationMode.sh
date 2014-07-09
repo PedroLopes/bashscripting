9d0c2d9a161e24472a815e8e42fd7b824a4e9342
pedro.lopes <pedro.lopes@hpi.uni-potsdam.de>
2014-07-09 17:56:20 +0200

    script that stops all stuff that could interrupt a powerpoint presentation, like dropbox and others

---
+++ presentationMode.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+        echo "please run ./presentationMode [start || stop]"
+	echo "[start] will kill Dropbox, Flux, Chrome and disable cronjobs"
+        echo "[stop] will restart Dropbox, Flux, Chrome and enable all previous cronjobs"
+	exit
+fi
+
+
+if [ "$1" = "start" ]
+then
+	#start 
+	echo "Stoppping Dropbox"
+	kill $(pgrep Dropbox)
+
+	echo "Stoppping Flux Screen Shader"
+	kill $(pgrep Flux)
+
+	echo "Resetting Crontab to Blank, saving current cron to file for backup"
+	touch blankcron.txt
+	crontab -l > cronbackup.txt
+	crontab blankcron.txt
+
+	echo "Killing Chrome"
+	ps -A | awk '/Google\ Chrome -psn_0_32526083/ { print $1 } ' | xargs kill -9
+
+fi
+
+if [ "$1" = "stop" ]
+then
+	#stop
+	echo "Populating Crontab to previous backup"
+	crontab cronbackup.txt
+	
+	echo "Starting Dropbox"
+	/Applications/Dropbox.app/Contents/MacOS/Dropbox &
+
+	echo "Starting Flux Screen Shader"
+	/Applications/Flux.app/Contents/MacOS/Flux &
+
+	echo "Chrome won't start by itself, please do it manually."
+fi
