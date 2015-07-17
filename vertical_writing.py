8696f4fb19e4888f126e7879d9ba246a46f9e2ae
Pedro Lopes <plopesresearch@gmail.com>
2015-07-17 19:06:38 +0200

    funny 2 liner

---
+++ vertical_writing.py
@@ -0,0 +1,13 @@
+import sys
+horizontal = ' '.join(sys.argv[1:])
+print('\n'.join(horizontal))
+
+#note to self: how cool is python, my first solution was (see below) and then second was (above)
+#vertical = []
+#for char in horizontal:
+#    vertical.append(char)
+#    vertical.append("\n")
+#vertical = ''.join(vertical)
+#print(vertical)
+
+
