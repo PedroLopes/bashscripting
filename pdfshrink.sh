63bbc3066fdb135bd072a84a9c3449937c678129
Pedro Lopes <plopesresearch@gmail.com>
2016-07-14 11:53:01 +0200

    improved the output of pdfshrink

--- pdfshrink.sh
+++ pdfshrink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo Shrinking pdf: $1 to $1_s.pdf with prepress option
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1_s.pdf $1
+echo Shrinking pdf: $1 to $(basename "$1" ".pdf")_s.pdf with prepress option
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$(basename "$1" ".pdf")_s.pdf $1
