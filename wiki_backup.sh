afa08d906c6619a7e94c760a72d96fddc251c081
Pedro Lopes <PedroLopes@users.noreply.github.com>
2023-12-31 09:40:38 -0600

    Update wiki_backup.sh
    info how to use

--- wiki_backup.sh
+++ wiki_backup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 tar zcpfv `(date +"%Y-%m-%d")`-dokuwiki-backup.tar.gz . --exclude='*dokuwiki-backup.tar.gz' --exclude='data/attic' --exclude='data/media_attic' --exclude='data/cache' --exclude='data/index' --exclude='data/locks' --exclude='data/tmp' --exclude='backup.sh';
+
+
+# Notes
+# This is a very minimal wiki backup script for dokuwiki.
+# It excludes directories that have old versions of files, cache, etc... as recommneded in the dokuwiki page. 
+# This allows the backup to be fully functional and yet smaller size. 
+# Note there are much more advanced scripts at https://www.dokuwiki.org/tips:backup_script so please use those if your needs are more advanced. 
